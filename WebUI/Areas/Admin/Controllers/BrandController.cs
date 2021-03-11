a4b8a4f510d931c6a2301ffee00aded8af746e7a
keroloussamy98 <keroloussamy98@gmail.com>
2021-03-11 21:59:43 +0200

    AddAreas

---
+++ WebUI/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Areas.Admin
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebUI/Areas/Dealer/DealerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebUI.Areas.Dealer
+{
+    public class DealerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Dealer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Dealer_default",
+                "Dealer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
